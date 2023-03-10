185236ef38c44fb7b7e8c986368f014771d0ad24
KillianDB <killiandomingues57@gmail.com>
2023-03-10 12:26:47 -0300

    setando router

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Importando express
 const express = require('express');
+const router = require('./router');
 
 // Criando app
 const app = express();
 
 // Configurando pasta de arquivos estáticos acessíveis
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // Configurando ejs como template engine
 app.set('view engine', 'ejs')
 
 // Importando e configurando router
-const router = require('./router');
 app.use(router);
 
 // Pondo app para ouvir em porta
